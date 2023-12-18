2f40bc98e2c129b7d0aa5aaea98fd8d8f9730b14
Inna Lapina <jyliana@gmail.com>
2023-12-19 00:08:03 +0200

    pagination - part 1

--- src/main/java/com/example/product/resolver/ManufacturerResolver.java
+++ src/main/java/com/example/product/resolver/ManufacturerResolver.java
@@ -6,6 +6,9 @@ import com.example.product.service.query.ManufacturerQueryService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
+import graphql.relay.Connection;
+import graphql.relay.SimpleListConnection;
+import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -21,5 +24,21 @@ public class ManufacturerResolver {
   public List<Manufacturer> manufacturers(@InputArgument Optional<ManufacturerInput> manufacturerInput) {
     return manufacturerQueryService.findManufacturers(manufacturerInput);
   }
+
+  @DgsQuery
+  public Connection<Manufacturer> manufacturersPagination(
+          @InputArgument Optional<ManufacturerInput> manufacturerInput,
+          DataFetchingEnvironment env,
+          @InputArgument Integer first,
+          @InputArgument Integer last,
+          @InputArgument String after,
+          @InputArgument String before
+  ) {
+    var fullResult = manufacturerQueryService.findManufacturers(manufacturerInput);
+
+    return new SimpleListConnection<>(fullResult).get(env);
+  }
+
+
 
 }
--- src/main/java/com/example/product/resolver/SeriesResolver.java
+++ src/main/java/com/example/product/resolver/SeriesResolver.java
@@ -6,7 +6,11 @@ import com.example.product.service.query.SeriesQueryService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
+import graphql.relay.SimpleListConnection;
+import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
+import graphql.relay.Connection;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -19,5 +23,19 @@ public class SeriesResolver {
   @DgsQuery
   public List<Series> series(@InputArgument Optional<SeriesInput> seriesInput) {
     return seriesQueryService.findSeries(seriesInput);
+  }
+
+  @DgsQuery
+  public Connection<Series> seriesPagination(
+          @InputArgument Optional<SeriesInput> seriesInput,
+          DataFetchingEnvironment env,
+          @InputArgument Integer first,
+          @InputArgument Integer last,
+          @InputArgument String after,
+          @InputArgument String before
+  ) {
+    var fullResult = seriesQueryService.findSeries(seriesInput);
+
+    return new SimpleListConnection<>(fullResult).get(env);
   }
 }
