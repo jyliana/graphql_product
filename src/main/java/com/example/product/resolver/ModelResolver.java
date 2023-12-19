b6c5a5da595640fc402cfc9d7902d8487b3620e1
Inna Lapina <jyliana@gmail.com>
2023-12-19 12:04:24 +0200

    pagination

--- src/main/java/com/example/product/resolver/ModelResolver.java
+++ src/main/java/com/example/product/resolver/ModelResolver.java
@@ -1,12 +1,15 @@
 package com.example.product.resolver;
 
 import com.example.graphql.types.ModelInput;
+import com.example.graphql.types.ModelPagination;
 import com.example.graphql.types.NumericComparisonInput;
 import com.example.product.datasource.entity.Model;
 import com.example.product.service.query.ModelQueryService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
+import graphql.relay.SimpleListConnection;
+import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -21,4 +24,31 @@ public class ModelResolver {
   public List<Model> models(@InputArgument Optional<ModelInput> modelInput, @InputArgument Optional<NumericComparisonInput> priceInput) {
     return modelQueryService.findModels(modelInput, priceInput);
   }
+
+  @DgsQuery
+  public ModelPagination modelsPagination(
+          @InputArgument Optional<ModelInput> modelInput,
+          @InputArgument Optional<NumericComparisonInput> priceInput,
+          DataFetchingEnvironment env,
+          @InputArgument Integer page,
+          @InputArgument Integer size
+  ) {
+    var pageModel = modelQueryService.findModels(
+            modelInput, priceInput, page, size
+    );
+
+    var paginatedResult = new ModelPagination();
+    var modelConnection = new SimpleListConnection<>(
+            pageModel.getContent()
+    ).get(env);
+
+    paginatedResult.setModelConnection(modelConnection);
+    paginatedResult.setPage(pageModel.getNumber());
+    paginatedResult.setSize(pageModel.getSize());
+    paginatedResult.setTotalPage(pageModel.getTotalPages());
+    paginatedResult.setTotalElement(pageModel.getTotalElements());
+
+    return paginatedResult;
+  }
+
 }
--- src/main/java/com/example/product/service/query/ModelQueryService.java
+++ src/main/java/com/example/product/service/query/ModelQueryService.java
@@ -8,6 +8,8 @@ import com.example.product.datasource.entity.Model;
 import com.example.product.datasource.repository.ModelRepository;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -87,6 +89,60 @@ public class ModelQueryService {
         yield priceBetween(value, highValue);
       }
     };
+  }
+
+  public Page<Model> findModels(Optional<ModelInput> input, Optional<NumericComparisonInput> priceInput, Integer page, Integer size) {
+    var modelInput = input.orElse(new ModelInput());
+    var seriesInput = modelInput.getSeries() == null ? new SeriesInput()
+            : modelInput.getSeries();
+    var manufacturerInput = seriesInput.getManufacturer() == null ?
+            new ManufacturerInput() : seriesInput.getManufacturer();
+
+    var priceSpecification = priceSpecificationFrom(priceInput);
+
+    var specification = Specification.where(
+            StringUtils.isNotBlank(manufacturerInput.getName()) ?
+                    manufacturerNameContainsIgnoreCase(
+                            manufacturerInput.getName()
+                    ) : null
+    ).and(
+            StringUtils.isNotBlank(manufacturerInput.getOriginCountry()) ?
+                    manufacturerOriginCountryContainsIgnoreCase(
+                            manufacturerInput.getOriginCountry()
+                    ) : null
+    ).and(
+            StringUtils.isNotBlank(seriesInput.getName()) ?
+                    seriesNameContainsIgnoreCase(
+                            seriesInput.getName()
+                    ) : null
+    ).and(
+            StringUtils.isNotBlank(modelInput.getName()) ?
+                    modelNameContainsIgnoreCase(
+                            modelInput.getName()
+                    ) : null
+    ).and(
+            modelInput.getIsAvailable() != null ?
+                    available(modelInput.getIsAvailable())
+                    : null
+    ).and(
+            modelInput.getTransmission() != null ?
+                    transmissionEquals(modelInput.getTransmission())
+                    : null
+    ).and(
+            !CollectionUtils.isEmpty(modelInput.getExteriorColors()) ?
+                    exteriorColorsLikeIgnoreCase(modelInput.getExteriorColors())
+                    : null
+    ).and(priceSpecification);
+
+    var sortOrders = sortOrdersFrom(modelInput.getSorts());
+
+    var pageable = PageRequest.of(
+            Optional.ofNullable(page).orElse(0),
+            Optional.ofNullable(size).orElse(50),
+            Sort.by(sortOrders)
+    );
+
+    return modelRepository.findAll(specification, pageable);
   }
 
 }
